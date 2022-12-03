3d7a461e3693fcc152821d30d0c738d6c7b6ac7a
eddiescj <eddiestudyifs@gmail.com>
2022-12-03 03:50:27 -0300

    using actuator for runtime config refresh

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.cloud:spring-cloud-config-client")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
--- src/main/kotlin/com/hrworker/handler/WorkerHandler.kt
+++ src/main/kotlin/com/hrworker/handler/WorkerHandler.kt
@@ -4,13 +4,14 @@ import com.hrworker.entities.Worker
 import com.hrworker.repository.WorkerRepository
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.core.env.Environment
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 class WorkerHandler(
